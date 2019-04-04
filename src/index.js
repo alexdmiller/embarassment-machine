f4857dbfe8ba763229216da7cbe3c6ad4c6b1bc3
Alex Miller <alexmiller@gmail.com>
2019-04-03 22:32:21 -0400

    broken bad very very bad bad data structure

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
   let lines = [];
   let matchLineIndices = [];
   let matchIdx = 0;
-
+  // array of positions -> maps of letters -> arrays of line indices
+  let letterMap = [];
 
   document.getElementById('msgDiv').innerHTML = 'Loading.................'
 
@@ -50,8 +51,36 @@ document.addEventListener("DOMContentLoaded", function() {
     if (e.keyCode == 8) {
       needle = needle.substring(0, needle.length - 1);
     } else {
+
+    // if (
+    //     (e.keycode > 47 && e.keycode < 58)   || // number keys
+    //     e.keycode == 32 || e.keycode == 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+    //     (e.keycode > 64 && e.keycode < 91)   || // letter keys
+    //     (e.keycode > 95 && e.keycode < 112)  || // numpad keys
+    //     (e.keycode > 185 && e.keycode < 193) || // ;=,-./` (in order)
+    //     (e.keycode > 218 && e.keycode < 223)) {
       needle += e.key;
     }
+
+    console.log('needle before', needle);
+    let needleWords = needle.split(' ');
+    let candidateLineIndices = [];
+
+    needleWords.forEach(word => {
+      [...word].forEach((char, charIndex) => {
+        candidateLineIndices = candidateLineIndices.concat(letterMap[charIndex][char]);
+      });
+
+      // if (candidateLineIndices.length > 0) {
+      //   candidateLineIndices = candidateLineIndices.reduce((a, b) => {
+      //     return a.filter(value => b.includes(value))
+      //   });
+      // }
+    })
+
+    candidateLineIndices = candidateLineIndices.filter((v, i, a) => a.indexOf(v) === i); 
+
+    console.log(candidateLineIndices);
 
     const pattern = `.+${needle}.+`
     const re = new RegExp(pattern, 'g')
@@ -59,20 +88,43 @@ document.addEventListener("DOMContentLoaded", function() {
     matchLineIndices = [];
     let line = ''
     matchIdx = 0;
-    lines.forEach((line, idx) => {
-      line = lines[idx];
+    candidateLineIndices.forEach(lineIndex => {
+      
+
+      line = lines[lineIndex];
       if(line.match(re)) {
-        matchLineIndices.push(idx);
+
+        matchLineIndices.push(lineIndex);
       }
     });
 
-    console.log(needle);
+    console.log("needle = ", needle);
   });
 
   fetch(corpusPath).then((res) => res.text()).then(data => {
-    document.getElementById('msgDiv').innerHTML = 'Searching............................'
+    console.log('parsing.....');
 
     lines = data.split('\n');
+
+    lines.forEach((line, lineIndex) => {
+      let words = line.split(' ');
+      words.forEach(word => {
+        [...word].forEach((char, charIndex) => {
+          if (typeof letterMap[charIndex] == 'undefined') {
+            letterMap[charIndex] = {};
+          }
+          
+          if (typeof letterMap[charIndex][char] == 'undefined') {
+            letterMap[charIndex][char] = [];
+          }
+
+
+          letterMap[charIndex][char].push(lineIndex);
+        });
+      });
+    });
+
+    document.getElementById('msgDiv').innerHTML = 'Searching............................'
 
     let lineIdx = 0;
     let skipFrameCounter = 0;
