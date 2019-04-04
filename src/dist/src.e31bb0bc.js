57280b4629e4239d45ac3c6c59384d351f212b2c
Alex Miller <alexmiller@gmail.com>
2019-04-03 21:04:13 -0400

    audio noise working

--- src/dist/src.e31bb0bc.js
+++ src/dist/src.e31bb0bc.js
@@ -5,8 +5,6 @@
 //
 // anything defined in a previous bundle is accessed via the
 // orig method which is the require for previous bundles
-
-// eslint-disable-next-line no-global-assign
 parcelRequire = (function (modules, cache, entry, globalName) {
   // Save the require from previous bundle to this closure if any
   var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
@@ -77,8 +75,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
     }, {}];
   };
 
+  var error;
   for (var i = 0; i < entry.length; i++) {
-    newRequire(entry[i]);
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
   }
 
   if (entry.length) {
@@ -103,6 +109,13 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
   return newRequire;
 })({"bigcorpus.txt":[function(require,module,exports) {
 module.exports = "/bigcorpus.97c74522.txt";
@@ -114,18 +127,47 @@ var _bigcorpus = _interopRequireDefault(require("./bigcorpus.txt"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 document.addEventListener("DOMContentLoaded", function () {
+  var needle = "love";
+  var lines = [];
   document.getElementById('msg').innerHTML = 'Loading.................';
+  document.addEventListener('click', function () {
+    function myPCMSource() {
+      return; // For example, generate noise samples.
+    }
+
+    var audioContext;
+
+    try {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      audioContext = new AudioContext();
+    } catch (e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+
+    var bufferSize = 4096;
+    var myPCMProcessingNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+
+    myPCMProcessingNode.onaudioprocess = function (e) {
+      var output = e.outputBuffer.getChannelData(0);
+
+      for (var i = 0; i < bufferSize; i++) {
+        // Generate and copy over PCM samples.
+        output[i] = Math.random() * 2 - 1;
+      }
+    };
+
+    myPCMProcessingNode.connect(audioContext.destination);
+  });
   fetch(_bigcorpus.default).then(function (res) {
     return res.text();
   }).then(function (data) {
     document.getElementById('msg').innerHTML = 'Searching............................';
-    var needle = 'love';
     var pattern = ".+".concat(needle, ".+");
     var re = new RegExp(pattern, 'g');
-    data = data.split('\n');
+    lines = data.split('\n');
     var matchLineIndices = [];
     var line = '';
-    data.forEach(function (line, idx) {
+    lines.forEach(function (line, idx) {
       line = data[idx];
 
       if (line.match(re)) {
@@ -134,7 +176,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-},{"./bigcorpus.txt":"bigcorpus.txt"}],"../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bigcorpus.txt":"bigcorpus.txt"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -156,26 +198,46 @@ function Module(moduleName) {
 }
 
 module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
 var parent = module.bundle.parent;
 
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60449" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52045" + '/');
 
   ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
     var data = JSON.parse(event.data);
 
     if (data.type === 'update') {
-      console.clear();
-      data.assets.forEach(function (asset) {
-        hmrApply(global.parcelRequire, asset);
-      });
+      var handled = false;
       data.assets.forEach(function (asset) {
         if (!asset.isNew) {
-          hmrAccept(global.parcelRequire, asset.id);
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
         }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
       });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else {
+        window.location.reload();
+      }
     }
 
     if (data.type === 'reload') {
@@ -263,7 +325,7 @@ function hmrApply(bundle, asset) {
   }
 }
 
-function hmrAccept(bundle, id) {
+function hmrAcceptCheck(bundle, id) {
   var modules = bundle.modules;
 
   if (!modules) {
@@ -271,9 +333,27 @@ function hmrAccept(bundle, id) {
   }
 
   if (!modules[id] && bundle.parent) {
-    return hmrAccept(bundle.parent, id);
+    return hmrAcceptCheck(bundle.parent, id);
   }
 
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
   var cached = bundle.cache[id];
   bundle.hotData = {};
 
@@ -298,10 +378,6 @@ function hmrAccept(bundle, id) {
 
     return true;
   }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAccept(global.parcelRequire, id);
-  });
 }
-},{}]},{},["../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.map
+},{}]},{},["../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+//# sourceMappingURL=/src.e31bb0bc.js.map
--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import corpusPath from './bigcorpus.txt'
 
 document.addEventListener("DOMContentLoaded", function() {
+  let needle = "love";
+  let lines = [];
+
   document.getElementById('msg').innerHTML = 'Loading.................'
+
+  document.addEventListener('click', () => {
+    function myPCMSource() {  
+      return    // For example, generate noise samples.
+    }
+
+    let audioContext;
+    try {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      audioContext = new AudioContext();
+    } catch(e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+
+    const bufferSize = 4096;
+    const myPCMProcessingNode = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    myPCMProcessingNode.onaudioprocess = function(e) {
+      const output = e.outputBuffer.getChannelData(0);
+      for (var i = 0; i < bufferSize; i++) {
+        // Generate and copy over PCM samples.
+        output[i] = Math.random() * 2 - 1;
+      }
+    }
+
+    myPCMProcessingNode.connect(audioContext.destination);
+  });
 
   fetch(corpusPath).then((res) => res.text()).then(data => {
     document.getElementById('msg').innerHTML = 'Searching............................'
 
-    const needle = 'love';
     const pattern = `.+${needle}.+`
     const re = new RegExp(pattern, 'g')
 
-    data = data.split('\n');
+    lines = data.split('\n');
 
     const matchLineIndices = [];
     let line = ''
-    data.forEach((line, idx) => {
+    lines.forEach((line, idx) => {
       line = data[idx];
       if(line.match(re)) {
         matchLineIndices.push(idx);
